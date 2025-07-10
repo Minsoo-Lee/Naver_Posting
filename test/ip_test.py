90371e8d7805ff007565398c3493035e4e603375
이민수 <minsoo.james1101@gmail.com>
2025-07-10 21:37:41 +0900

    current_ip argument added

--- test/ip_test.py
+++ test/ip_test.py
@@ -15,7 +15,7 @@ panel = wx.Panel(frame)
 top_button = wx.Button(panel, label="현재 IP 확인")
 bottom_button = wx.Button(panel, label="비행기모드 on/off")
 
-def current_ip():
+def current_ip(event):
     print(ip_trans.get_current_ip())
 
 # 이벤트 바인딩
