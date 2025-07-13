de8630fb911c38c155e936e0cad210948d15a421
이민수 <minsoo.james1101@gmail.com>
2025-07-14 01:08:51 +0900

    enter_posting_window fixed

--- web/webdriver.py
+++ web/webdriver.py
@@ -61,7 +61,7 @@ def click_element_css(css):
     driver.find_element(By.CSS_SELECTOR, css).click()
 
 def click_element_link_text(link_text):
-    driver.find_element(By.LINK_TEXT, "글쓰기").click()
+    driver.find_element(By.LINK_TEXT, link_text).click()
 
 def get_element_xpath(xpath):
     return driver.find_element(By.XPATH, xpath)
