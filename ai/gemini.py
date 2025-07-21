bf328c9d43498337a3e609462e0af5bc74083656
이민수 <minsoo.james1101@gmail.com>
2025-07-22 02:01:23 +0900

    drawing border fixed

--- ai/gemini.py
+++ ai/gemini.py
@@ -1,8 +1,10 @@
-import dotenv
+import re
+
+from ui import log
 import google.generativeai as genai
-from dotenv import load_dotenv
+from google.api_core.exceptions import ResourceExhausted
+
 from data import text_data
-import os
 
 # gemini_key = "AIzaSyDo6wlM9Q6SFKS-rpHoS_sJQabVt9OEDnI"
 model = None
@@ -20,27 +22,34 @@ def init_gemini():
 
 def create_content(contents, address, company):
     global model
-
-    response = model.generate_content(f"""
-            내가 글을 쓸건데, 키워드는 {address}, {company}야.
-            예시 글들을 보여줄게.
-            
-            예시 1:
-            {contents[0]}
+    try:
+        response = model.generate_content(f"""
+                내가 글을 쓸건데, 키워드는 {address}, {company}야.
+                예시 글들을 보여줄게.
+                
+                예시 1:
+                {contents[0]}
+        
+                예시 2:
+                {contents[1]}
     
-            예시 2:
-            {contents[1]}
+                중간에 사진을 5장 넣을 건데, 너가 생성한 글에서 사진을 넣을 만한 장소에 %사진% 이라고 써 주고, 1500자 내외의 글로 작성해 줘.
+                문장이 . ? ! 이런 끝맺음 기호로 끝날 때마다 줄바꿈은 꼭 해줘야 해.
+                사진이 들어가는 공간은 문맥을 해치지 말아야 해. 예를 들면 본문이 하나 끝나고, 소제목이 들어가기 전에 넣어주면 좋겠어. 
+                그리고 사진에 대한 설명을 적으면 글을 파싱하기 어려우니까, 사진에 대한 설명은 반드시 빼 줘.
+                연락처, 주소, 홈페이지 같은 정보는 적지 않아도 돼
+                또한, 인사말과 끝맺음말은 내가 직접 적을 거니까 그건 빼줘.
+                그리고 **과 같은 마크다운 언어는 쓰지 마.
+                .""")
 
-            중간에 사진을 5장 넣을 건데, 너가 생성한 글에서 사진을 넣을 만한 장소에 %사진% 이라고 써 주고, 1500자 내외의 글로 작성해 줘.
-            문장이 . ? ! 이런 끝맺음 기호로 끝날 때마다 줄바꿈은 꼭 해줘야 해.
-            사진이 들어가는 공간은 문맥을 해치지 말아야 해. 예를 들면 본문이 하나 끝나고, 소제목이 들어가기 전에 넣어주면 좋겠어. 
-            그리고 사진에 대한 설명을 적으면 글을 파싱하기 어려우니까, 사진에 대한 설명은 반드시 빼 줘.
-            연락처, 주소, 홈페이지 같은 정보는 적지 않아도 돼
-            또한, 인사말과 끝맺음말은 내가 직접 적을 거니까 그건 빼줘.
-            그리고 **과 같은 마크다운 언어는 쓰지 마.
-            .""")
-
-    return response.text
+        return response.text
+    except ResourceExhausted as e:
+        match = re.search(r'quota_id: "(.*?)"', str(e))
+        if match:
+            quota_id = match.group(1)
+            log.append_log("[ERROR] 무료 요금제의 하루 일일 요청을 초과하였습니다.\nquota_id: {quota_id}")
+            log.append_log("[ERROR] 충분한 시간이 흐른 뒤에 프로그램을 재시작해 주세요.")
+        raise
 
 def create_title(address, company, article):
     global model
