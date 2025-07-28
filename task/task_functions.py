e079d29904db4d6e77f689095f85abe4caf11a42
이민수 <minsoo.james1101@gmail.com>
2025-07-28 20:33:20 +0900

    image_index fixed

--- task/task_functions.py
+++ task/task_functions.py
@@ -160,10 +160,11 @@ def write_content_blog(address, company, article, image_path, image_length):
             try:
                 image.draw_border_sample(image_path[image_index])
                 image.upload_image(NEW_IMAGE_PATH)
-                image_index += 1
                 image.remove_image(NEW_IMAGE_PATH)
             except FileNotFoundError:
                 log.append_log(f"[ERROR] 이미지 경로 [{image_path[image_index]}]를 찾을 수 없습니다. 다음 작업으로 넘어갑니다.")
+            finally:
+                image_index += 1
         elif VIDEO in content:
             # 썸네일 사진을 이용한 영상을 업로드
             video_path = os.path.abspath(VIDEO_PATH)
